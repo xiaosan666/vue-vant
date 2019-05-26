cc30ca46d8fabd01889fd3846a93969d458adccf
yanxiaojun617 <yanxiaojun617@163.com>
2019-05-26 16:16:35 +0800

    init

---
+++ src/views/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-about">
+    about
+  </div>
+</template>
+<script>
+export default {
+  name: 'about',
+  data() {
+    return {
+    };
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      console.log('init: about');
+    }
+  }
+};
+</script>
+<style lang="scss">
+.page-about {
+
+}
+</style>
+
---
+++ src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      console.log('home init');
+    }
+  }
+};
+</script>
